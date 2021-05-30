f1aab0316883623bc796879b22f041c7584a3b8e
Sanjeev Singh <singh.sanjeevr@gmail.com>
2021-05-30 12:48:35 +0530

    add deployment script

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = { 
+  apps: [{
+    name    : "trying",
+    script  : "npm",
+    args    : "start"
+  }]
+}
--- index.js
+++ index.js
@@ -8,5 +8,6 @@ app.get('/', function (req, res) {
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
- 
+ console.log(process.env.PORT);
+ console.log('here');
 module.exports = app;
